67eddd4a936b72e13695cfd91a9abb43628c1133
maiaarrais <maiacarrais@gmail.com>
2025-11-16 00:59:01 -0500

    make the scene change work

--- Assets/Hover.cs
+++ Assets/Hover.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
 
 public class Hover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
@@ -7,9 +8,16 @@ public class Hover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
     public GameObject[] ImageAppear; 
     [SerializeField] private string option;
 
+    // Public variable to set the target scene index in the Inspector
+    public int sceneIndex = 2;
+
     void Start()
     {
         manager = GameManager.Instance;
+        if (manager == null)
+        {
+            Debug.LogError("Hover: GameManager instance not found.");
+        }
     }
 
     public void OnPointerEnter(PointerEventData eventData)
@@ -24,6 +32,15 @@ public class Hover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 
     void OnMouseDown()
     {
-        manager.AddPoints(option, 2);
+        // Check if manager exists
+        if (manager != null)
+        {
+            manager.AddPoints(option, 2);
+            SceneManager.LoadSceneAsync(sceneIndex);
+        }
+        else
+        {
+            Debug.LogWarning("GameManager is not available!");
+        }
     }
 }
