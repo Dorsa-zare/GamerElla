3d73e171c07a302863f1313a23631443f5f40763
Dorsa Zare <D_zare@live.concordia.ca>
2025-11-16 16:02:46 -0500

    image appear

--- Assets/UI/Script/MainMenu.cs
+++ Assets/UI/Script/MainMenu.cs
@@ -2,9 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-public class MainMenu : MonoBehaviour
+using UnityEngine.EventSystems;
+public class MainMenu : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
 
+    public GameObject[] ImageAppear; 
     public void PlayGame()
     {
         SceneManager.LoadSceneAsync(1);
@@ -13,6 +15,17 @@ public class MainMenu : MonoBehaviour
     public void QuitGame()
     {
         Application.Quit();
+    }
+    
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        foreach (GameObject image in ImageAppear) image.SetActive(true);
+        
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        foreach (GameObject image in ImageAppear) image.SetActive(true);
     }
 }
 
